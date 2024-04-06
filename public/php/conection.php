389d0e6c29500055063acc5d2e604108c30f8832
toledanogus <toledanogu@gmail.com>
2024-04-06 14:58:39 -0600

    Version 1

--- public/php/conection.php
+++ public/php/conection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'tareas') or die(mysqli_error($conn));
-/* $conn = mysqli_connect('localhost', 'toledan1_gus', 'sfreud1978', 'toledan1_pedrueza') or die(mysqli_error($conn)); */
+/* $conn = mysqli_connect('localhost', 'root', '', 'tareas') or die(mysqli_error($conn)); */
+$conn = mysqli_connect('localhost', 'toledan1_gus', 'sfreud1978', 'toledan1_pedrueza') or die(mysqli_error($conn));
 ?>
